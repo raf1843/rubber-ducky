719113b2fe5592a4a6b6a5f3f960c89878de063a
Raina Freeman <90727927+raf1843@users.noreply.github.com>
2021-09-29 16:14:11 -0400

    Add files via upload
    Week 1 and 2

---
+++ writeup2/ducky-demo.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# proof of entry
+cd ~/Desktop
+python3 -mwebbrowser https://www.youtube.com/watch?v=MtN1YnoL46Q
+echo 'quack quack' > quackpack.txt
---
+++ writeup2/ducky-demo.bat
@@ -0,0 +1,4 @@
+
+cd %userprofile%\OneDrive\Desktop
+ECHO quack quack > quackpack.txt
+rem start https://www.youtube.com/watch?v=MtN1YnoL46Q
