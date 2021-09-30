dd35483c6b9c893d411d6acfe3966cb7d1ad84e0
Raina Freeman <90727927+raf1843@users.noreply.github.com>
2021-09-29 20:35:24 -0400

    Add files via upload
    Forgot to remove debugs from ducky-demo.bat, fixed

--- writeup2/ducky-demo.bat
+++ writeup2/ducky-demo.bat
@@ -1,4 +1,4 @@
-
+ECHO OFF
 cd %userprofile%\OneDrive\Desktop
 ECHO quack quack > quackpack.txt
-rem start https://www.youtube.com/watch?v=MtN1YnoL46Q
+start https://www.youtube.com/watch?v=MtN1YnoL46Q
