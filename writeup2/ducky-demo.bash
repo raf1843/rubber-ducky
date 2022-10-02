5f3fb2ab4a60bec0aef0fca581cf42ddc32f5eef
Raina Freeman <90727927+raf1843@users.noreply.github.com>
2022-10-02 18:56:47 -0400

    Update ducky-demo.bash

--- writeup2/ducky-demo.bash
+++ writeup2/ducky-demo.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # proof of entry
 cd ~/Desktop
+# alternatively xdg-open if python isn't installed
 python3 -mwebbrowser https://www.youtube.com/watch?v=MtN1YnoL46Q
 echo 'quack quack' > quackpack.txt
